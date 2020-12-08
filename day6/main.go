34d162a04b5609209493a3558253685ece42d933
eric <eric@eric.eric>
2020-12-08 20:41:31 +0100

    Bug correction

--- day6/main.go
+++ day6/main.go
@@ -12,17 +12,24 @@ func main() {
 	all, _ := ioutil.ReadAll(open)
 
 	totalSum := 0
-	responses := map[string]bool{}
-	for _, group := range strings.Split(string(all), "\n\n") {
-		for _, e := range group {
-			if e != '\n' {
-				responses[string(e)] = true
+	for _, groupChoice := range strings.Split(string(all), "\n\n") {
+		choices := map[string]bool{}
+		for i, peopleChoice := range strings.Split(groupChoice, "\n") {
+			if i == 0 { // first guy
+				for _, char := range peopleChoice {
+					choices[string(char)] = true
+				}
+			} else { // others
+				old := choices
+				choices = map[string]bool{}
+				for _, char := range peopleChoice {
+					if old[string(char)] {
+						choices[string(char)] = true
+					}
+				}
 			}
 		}
-		totalSum += len(responses)
-		responses = map[string]bool{}
+		totalSum += len(choices)
 	}
-
 	println(totalSum)
-
 }
