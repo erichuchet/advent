672b0e5a5891f3c1689229a8bffba9e353d17cff
eric <eric@eric.eric>
2020-12-09 22:27:19 +0100

    day7 second method

--- day7/main.go
+++ day7/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -12,27 +11,22 @@ func main() {
 	open, _ := os.Open("day7/inputs.txt")
 	all, _ := ioutil.ReadAll(open)
 
-	re, _ := regexp.Compile("(\\w+\\s\\w+)\\s(bags|bag)")
-	colors := map[string][]string{}
-	for _, line := range strings.Split(string(all), "\n") {
-		allmatches := re.FindAllStringSubmatch(line, -1)
-		if allmatches != nil {
-			extern := allmatches[0][1]
-			for _, intern := range allmatches[1:] {
-				color := intern[1]
-				colors[color] = append(colors[color], extern)
-			}
-		}
-	}
-
+	lines := strings.Split(string(all), "\n")
 	seen := map[string]bool{}
 	stack := []string{"shiny gold"}
 	for len(stack) > 0 {
-		containers := colors[stack[0]]
-		stack = append(stack[1:], containers...)
-		for _, color := range containers {
-			seen[color] = true
+		current := stack[0]
+		for _, line := range lines {
+			index := strings.Index(line, current)
+			if index > 0 {
+				container := line[:strings.Index(line, " bag")]
+				if !seen[container] {
+					seen[container] = true
+					stack = append(stack, container)
+				}
+			}
 		}
+		stack = stack[1:]
 	}
 
 	fmt.Println("Shiny gold : ", len(seen))
